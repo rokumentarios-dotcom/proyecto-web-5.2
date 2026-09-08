f90ba59e09b9a15ca78e2da58c5cf71b29bf9117
rokumentarios-dotcom <rokumentarios@gmail.com>
2026-09-08 14:49:58 -0600

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -6,7 +6,7 @@ import urllib.parse
 import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
-import libsql_experimental  # <--- Agregada
+import libsql  # <--- Agregada
 
 # ==========================================
 # CONFIGURACIÓN DE BASE DE DATOS (Nube / Local)
